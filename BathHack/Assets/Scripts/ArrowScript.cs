13bbebb2526e0f714b61d0c97c092a6e7dd16d89
ChrisPrendergast <78866705+ChrisPrendergast@users.noreply.github.com>
2023-04-02 11:38:19 +0100

    The Project
    :)

---
+++ BathHack/Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowScript : MonoBehaviour
+{
+    private RectTransform rTransform;
+    public float speed = 1;
+    public int val = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rTransform = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rTransform.anchoredPosition = new Vector2(rTransform.anchoredPosition.x, rTransform.anchoredPosition.y - speed * Time.deltaTime);
+    }
+}
---
+++ BathHack/Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene(1);
+    }
+}
